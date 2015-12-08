33da7b14e706ccc9a32225428431107472366f59
Yeya <yeeeeyaaaa@gmail.com>
2015-12-08 21:35:00 +0100

    Initial commit

---
+++ controllers/arduinoYun.js
@@ -0,0 +1,51 @@
+var http = require('http');
+
+var Url = function(options) {
+    console.log('info', 'initializing');
+    this.debug = options && options.debug || false;
+    this.host = options || 'http://192.168.1.51';
+    this.port = options || '8080'
+    this.pin = options || '1';
+    this.call = options || 'analog';
+}
+
+module.exports = function(app, options) {
+    console.log('info', 'attempting to find Arduino Wifi board');
+    var url = new Url(options);
+
+    /*http.get(url.host + ':' + url.port + '/' + url.call + '/' + url.pin, function(data) {
+        console.log(data);
+    });*/
+
+    var options = {
+        host: url.host,
+        path: '/' + url.call + '/' + url.pin,
+        //since we are listening on a custom port, we need to specify it by hand
+        port: url.port,
+        //This is what changes the request to a POST request
+        method: 'GET'
+    };
+
+    http.get(options, function(res) {
+        var body = '';
+        res.on('data', function(chunk) {
+            //self.emit('connected');
+            body += chunk;
+        });
+        res.on('end', function() {
+            console.log(body);
+            /*self.emit('data', body);
+            var err = null;
+            self.emit('read', err, body);
+            self.emit('ready');*/
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+        var msg = "Could not establish HTTP connection to Arduino.";
+        /*if (self.listeners('error').length > 0) {
+          self.emit('error', msg);
+        } else {*/
+        console.error(msg);
+        //}
+    });
+};
---
+++ domoticon.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cons = require('consolidate');
+var swig = require('swig');
+var path = require('path');
+//var Conf = require('./config/development.json');
+
+
+// Load configuration
+/*var node_env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+console.log('node env ' + node_env);
+var config = Conf.load(node_env);*/
+/*var config = Conf;
+
+if (!config.host || !config.port || !config.secret || !config.mongo || !config.mongo.name || !config.mongo.host || !config.mongo.port || !config.mongo.user) {
+    return console.log('\u001b[31mMissing configuration file \u001b[33mconfig/' + node_env + ' recupera algo ' + config.host + '.json\u001b[31m. Create configuration file or start with `NODE_ENV=development node domoticon.js` to use another configuration file.\033[0m');
+}*/
+var config;
+var options = {
+    debug: config && config.debug || false,
+    host: config && config.host || 'http://192.168.1.51',
+    port: config && config.port || '8080',
+    pin: config && config.pin || '1',
+    call: config && config.call || 'analog'
+}
+
+var app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.engine('server.view.html', cons['swig']);
+app.set('view engine', 'server.view.html');
+app.set('views', './views');
+app.use(express.static(path.resolve('./public')));
+
+app.route('/domoticon')
+    .get(function(req, res, next) {
+        res.render('index');
+    });
+
+//require('./controllers/leds')(app);
+
+require('./controllers/arduinoYun')(app);
+
+app.listen(8080, function() {
+    console.log('Raspi Express server listening...');
+});
